6a929c146231deecece19b460715be29052d07ac
zhangqiang <zqiang_0303@outlook.com>
2019-05-16 10:54:29 +0800

    获取昨天日期

--- qiang11_instance/q06_calendar.py
+++ qiang11_instance/q06_calendar.py
@@ -5,6 +5,7 @@ import calendar
 #
 # cale = calendar.month(yy, mm)  # 输出日历
 # print(cale)
+import datetime
 
 cale1 = calendar.month(2019, 6)  # 打印日历
 print(cale1)
@@ -12,3 +13,16 @@ print(cale1)
 
 month_range = calendar.monthrange(2019, 6)  # 打印日历天数,返回元阻,第一参数(0-6)对应该星期几
 print(month_range)
+
+
+
+def getYesterday():
+    '''获取昨天日期'''
+
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=1)
+    yesterday = today - oneday
+    return yesterday
+
+
+print(getYesterday())
