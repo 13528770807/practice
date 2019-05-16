80250a12f621c6f6d187d15087f993cb4e6dec3a
zhangqiang <zqiang_0303@outlook.com>
2019-05-16 10:03:32 +0800

    字符串判断

---
+++ qiang11_instance/q10_str_judge.py
@@ -0,0 +1,11 @@
+
+
+str = 'hello world'
+print(str.islower())  # 判断所有字符小写
+print(str.isupper())  # 判断所有字符大写
+print(str.istitle())  # 判断所有字符首字母大写,像标题
+print(str.isalpha())  # 判断所有的字符都是字母
+print(str.isdigit())  # 判断所有字符都是数字
+print(str.isalnum())  # 判断所有字符都是字母和数字
+print(str.isspace())  # 判断所有字符都是空白字符, \t \n \r
+print(str)
