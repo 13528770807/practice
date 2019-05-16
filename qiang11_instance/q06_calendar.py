84ca8234ff61b5059be82d355e7aebfb8f08625a
zhangqiang <zqiang_0303@outlook.com>
2019-05-16 10:32:36 +0800

    打印日历天数

--- qiang11_instance/q06_calendar.py
+++ qiang11_instance/q06_calendar.py
@@ -1,11 +1,14 @@
 import calendar
 
-yy = int(input("请输入年份:"))
-mm = int(input("请输入月份:"))
+# yy = int(input("请输入年份:"))
+# mm = int(input("请输入月份:"))
+#
+# cale = calendar.month(yy, mm)  # 输出日历
+# print(cale)
 
-cale = calendar.month(yy, mm)  # 输出日历
-print(cale)
-
-cale1 = calendar.month(2019, 6)
+cale1 = calendar.month(2019, 6)  # 打印日历
 print(cale1)
 
+
+month_range = calendar.monthrange(2019, 6)  # 打印日历天数,返回元阻,第一参数(0-6)对应该星期几
+print(month_range)
