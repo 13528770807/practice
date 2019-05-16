982f5fdc448929dbfd421479bf323ef0576cdac2
zhangqiang <zqiang_0303@outlook.com>
2019-05-16 10:15:45 +0800

    字符串转换

--- qiang11_instance/q10_str_judge.py
+++ qiang11_instance/q10_str_judge.py
@@ -1,6 +1,7 @@
 
 
 str = 'hello world'
+# 判断字符串
 print(str.islower())  # 判断所有字符小写
 print(str.isupper())  # 判断所有字符大写
 print(str.istitle())  # 判断所有字符首字母大写,像标题
@@ -9,3 +10,11 @@ print(str.isdigit())  # 判断所有字符都是数字
 print(str.isalnum())  # 判断所有字符都是字母和数字
 print(str.isspace())  # 判断所有字符都是空白字符, \t \n \r
 print(str)
+
+print('='*60)
+
+# 字符串转换
+print(str.upper())  # 将所有字符小写字母转为大写字母
+print(str.lower())  # 将所有字符大写字母转为小写字母
+print(str.title())  # 将所有单词首字母转为大写, 其余小写
+print(str.capitalize())  # 将第一个单词首字母转为大写, 其余小写
