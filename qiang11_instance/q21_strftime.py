6bed63fb8fcf26a7737b22f13c65a90a8ee1b984
zhangqiang <zqiang_0303@outlook.com>
2019-05-23 10:51:56 +0800

    将时间戳转换为指定格式日期2

--- qiang11_instance/q21_strftime.py
+++ qiang11_instance/q21_strftime.py
@@ -3,17 +3,27 @@ import time
 
 
 # instance 1
+# 获得当前时间时间戳
 now = int(time.time())
 print(now)
+#转换为其他日期格式,如:"%Y-%m-%d %H:%M:%S"
 timeArray = time.localtime(now)
 print('timeArray:', timeArray)
 otherStyletime = time.strftime('%Y-%m-%d %H:%M:%S', timeArray)
 print(otherStyletime)
 
 
+# timeStamp = 1557502800
+# timeArray = time.localtime(timeStamp)
+# otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+# print(otherStyleTime)
+
+
 # instance 2
+# 获得当前时间
 now = datetime.datetime.now()
-print(now)
+print('now:', now)
+# 转换为指定的格式
 otherStyletime2 = now.strftime("%Y-%m-%d %H:%M:%S")
 print(otherStyletime2)
 
--- qiang11_instance/q22_yesterday_time.py
+++ qiang11_instance/q22_yesterday_time.py
@@ -1,2 +1,22 @@
+import datetime
+import time
 
 
+# instance 1
+# 先获得时间数组格式的日期
+threeDaysAgo = datetime.datetime.now() - datetime.timedelta(days=3)
+print(threeDaysAgo)
+
+# 转换为时间戳
+timeStamp = time.mktime(threeDaysAgo.timetuple())
+print(timeStamp)
+
+# 转换为其他字符串格式
+otherStyleTime = threeDaysAgo.strftime('%Y-%m-%d %H:%M:%S')
+print(otherStyleTime)
+
+
+# instance 2
+timeStamp2 = 1557502800
+dateArray = datetime.datetime.utcfromtimestamp(timeStamp2)
+print('dateArray:', dateArray)
