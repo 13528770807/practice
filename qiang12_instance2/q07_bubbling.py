cec4435de5c1e4e371733bbb001afa0588914e3a
zhangqiang <zqiang_0303@outlook.com>
2019-05-24 15:20:12 +0800

    冒泡排序1

--- qiang12_instance2/q07_bubbling.py
+++ qiang12_instance2/q07_bubbling.py
@@ -1,5 +1,5 @@
 
-
+"""
 def bubbling_sort(arr):
     for i in range(len(arr)-1, 0, -1):
         for j in range(i):
@@ -11,3 +11,26 @@ if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
     bubbling_sort(arr)
     print(arr)
+"""
+
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    # 遍历所有数组元素
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print("排序后的数组:")
+for i in range(len(arr)):
+    print("%d" % arr[i]),
