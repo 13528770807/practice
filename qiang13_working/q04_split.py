7c92de4c5c12e2ca45330f3c04b2b5dc1dac4a06
zhangqiang <zqiang_0303@outlook.com>
2019-05-27 17:44:56 +0800

    split() 指定字符串切割

---
+++ qiang13_working/q04_split.py
@@ -0,0 +1,25 @@
+
+
+b = 'hello,wor*ldhh'
+print(b.split("*"))  # 字符串内指定符号切割
+
+li = list()
+a = ["啊啊啊啊啊", "呃呃呃。鹅鹅鹅鹅鹅鹅饿"]  # 列表内指定符号切割
+for i in range(0, len(a)):
+    # li.append(a[i].split("。"))
+    li = li + a[i].split('。')
+
+print(li)
+
+lis = ['aaaj']
+lis = lis + ['bbb']  # 列表可以用 "+" 号相加 相当于 extend() 方法
+print(lis)
+
+li1 = [1, 2, 3]
+li2 = [6, 7, 8, 9]
+# li1.append(li2)
+print(li1)  # [1, 2, 3, [6, 7, 8, 9]]
+# li1.extend(li2)
+print(li1)  # [1, 2, 3, 6, 7, 8, 9]
+li1 = li1 + li2
+print(li1)  # [1, 2, 3, 6, 7, 8, 9]
