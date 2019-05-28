51e6a76d7e872ee9eb0d0b359199e3b5354311e6
13528770807 <zqiang_0303@outlook.com>
2019-05-28 21:12:15 +0800

    列表相加去重(含字符串类型的数字)

--- qiang13_working/q07_merge_list.py
+++ qiang13_working/q07_merge_list.py
@@ -9,23 +9,37 @@ for i in range(len(li1)):
     if li1[i] not in li2:
         li2.append(li1[i])
 
-print('li2:', li2)
+print('li2:', li2)  # li2: [2, 3, 4, 5, 67, 8, 89, 9, 34, 666, 1, 6]
 
 
-# extend
+# extend  列表相加
 li1.extend(li2)
-print('li1:', li1)
+print('li1:', li1)  # li1: [1, 2, 34, 5, 6, 2, 3, 4, 5, 67, 8, 89, 9, 34, 666, 1, 6]
 
 
 # 方法二
 lst2 = [1, 2, 34, 5, 6, 2, 3, 4, 5, 67, 8, 89, 9, 34, 666, 9]
 n = 0
-while n < len(lst2):
+while n < len(lst2):  # 逐个遍历, 规避 for 循环漏掉
     for i in lst2:
         if lst2.count(i) > 1:
             lst2.remove(i)
         n += 1
 
-print('lst2:', lst2)
+print('lst2:', lst2)  # lst2: [1, 34, 6, 2, 3, 4, 5, 67, 8, 89, 34, 666, 9]
 
+
+l1 = [1, 2, 3, "4", 5]
+# l2 = [1, 3, 4, 7]
+
+
+list1 = str(l1)
+str1 = 'aaa'
+str2 = list1 + str1
+print(type(list1))  # <class 'str'>
+print(str2)  # [1, 2, 3, '4', 5]aaa
+
+
+ali = [int(x) for x in l1]  # 通过列表推导式 将含有字符串的列表 转为整型数字列表
+print('ali:', ali)  # ali: [1, 2, 3, 4, 5]
 
