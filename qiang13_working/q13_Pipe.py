2a5115a72691cfbe2173cfc4bd1d83033406dc2f
zhangqiang <zqiang_0303@outlook.com>
2019-05-29 18:21:34 +0800

    进程传递-管道

---
+++ qiang13_working/q13_Pipe.py
@@ -0,0 +1,24 @@
+from multiprocessing import Pipe, Process
+
+# 子进程函数 执行方法
+def foo(subconn):
+    print('from parent_conn:', subconn.recv())
+    subconn.send('吃了的')
+
+
+if __name__ == "__main__":
+    # 创建管道两端
+    parent_conn, child_conn = Pipe()
+
+    # 创建子进程
+    p = Process(target=foo, args=(child_conn, ))
+
+    # 运行子进程
+    p.start()
+
+    # 进程间数据传递
+    parent_conn.send('吃了么')
+    print('from chile_conn:', parent_conn.recv())
+
+    # 等待进程执行完后关闭
+    p.join()
