d4a030e2edfea2926a305185792241475f4fda0e
zhangqiang <zqiang_0303@outlook.com>
2019-05-30 17:02:37 +0800

    闭包+装饰器2

--- qiang13_working/q16_wrapper.py
+++ qiang13_working/q16_wrapper.py
@@ -1,6 +1,9 @@
 
 
 # 实例一
+import time
+
+
 def f1(x):
     print('4 >> x:', x)
     return x * x
@@ -40,3 +43,62 @@ def foo(n, m):
 foo1 = wrapper(foo)  # foo1 实际就是 inner ; (foo1对象名,也可以写成 foo)
 print(foo1(1, 2))  # 调用 inner 函数
 # print(foo(1, 2))
+
+
+print('='*60)
+
+
+def performance(unit):
+    def performance_decorator(f):
+        def foo(*args, **kwargs):
+            start = time.time()
+            fun = f(*args, **kwargs)
+            end = time.time()
+            print('fun_time:%f' % (end - start), unit)
+            return fun
+        return foo
+    return performance_decorator
+
+
+@performance('zhang')  # 带有参数的装饰器
+def test_foo(n):
+    fibo_li = list()
+    a, b = 0, 1
+    for i in range(n):
+        fibo_li.append(a)
+        a, b = b, a+b
+    return fibo_li
+
+
+print(test_foo(10))
+
+
+print('*'*60)
+
+
+from functools import reduce
+
+
+def performance(unit):
+    def performance_decorator(f):
+        def p(x):
+            t1 = time.time()
+            r = f(x)
+            t2 = time.time()
+            print('call %s() in %f %s' % (f.__name__, t2 - t1, unit))
+            return r
+
+        return p
+
+    return performance_decorator
+
+
+@performance('ms')
+def factorial(n):
+    return reduce(lambda x, y: x * y, range(1, n + 1))
+
+
+print(factorial(4))
+
+
+
