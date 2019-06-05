693dc2a47ef5cd5be8a2bacd2510726b89281a78
13528770807 <zqiang_0303@outlook.com>
2019-06-05 21:51:10 +0800

    合并列表

--- qiang14_working2/q04_extend.py
+++ qiang14_working2/q04_extend.py
@@ -58,7 +58,44 @@ s1 = re.findall('<a>(.*)</a>', s)
 s2 = re.findall('<a>(.*?)</a>', s)
 print(s1)  # ['hehe</a><a>haha']
 print(s2)  # ['hehe', 'haha']
+print('='*60)
 
 
 # 38、简述Django的orm
+'''
+ORM，全拼Object-Relation Mapping，意为对象-关系映射
 
+实现了数据模型与数据库的解耦，通过简单的配置就可以轻松更换数据库，
+而不需要修改代码只需要面向对象编程,orm操作本质上会根据对接的数据库引擎，
+翻译成对应的sql语句,所有使用Django开发的项目无需关心程序底层使用的是MySQL、Oracle、sqlite....，
+如果数据库迁移，只需要更换Django的数据库引擎即可
+'''
+
+
+# 39 [[1, 2], [3, 4], [5, 6]] 一行代吗得 [1, 2, 3, 4, 5, 6]
+# 方法一
+li3 = [[1, 2], [3, 4], [5, 6]]
+new_li3 = []
+for i in li3:
+    for x in i:
+        new_li3.append(x)
+
+print(new_li3)
+
+# 方法二
+res = [x for i in li3 for x in i]
+print(res)
+
+# import numpy as np
+# res1 = np.array(li3).flatten().tolist()
+# print(res1)
+
+
+# 40, x = "abc", y = "def", z = ["d", "e", "f"] 求 x.join(y), x.join(z)
+x = "abc"
+y = "def"
+z = ["d", "e", "f"]
+resu = x.join(y)  # join()括号中传入可遗代对象, x 做为参数传进去, os.path.join() 也经常用到
+resu1 = x.join(z)
+print(resu)  # dabceabcf
+print(resu1)  # dabceabcf
