88f242aabfd0469b50b58ff2af649f4ac2b2d7c1
zhangqiang <zqiang_0303@outlook.com>
2019-06-14 11:44:14 +0800

    celery应用１

--- qiang14_working2/q11_args_kwargs.py
+++ qiang14_working2/q11_args_kwargs.py
@@ -1,2 +1,28 @@
 
 
+import random
+
+
+li = [1, 2, 3, 4, 5, 6]
+random.shuffle(li)  # 随机(洗牌)
+print(li)
+
+str1 = " abcd"
+str2 = "abCd "
+print(str1.lstrip())  # 去掉左边空格
+print(str2.rstrip())  # 去掉右边空格
+
+print(str1.upper().lstrip())
+print(str2.lower())
+
+print(str1.islower())
+print(str2.isupper())
+
+try:
+    if 1 != "1":
+        print('ture')
+    else:
+        print('error')
+except Exception as e:
+    print('e error')
+    print(e)
--- qiang15_discuss/q01_celery.py
+++ qiang15_discuss/q01_celery.py
@@ -2,18 +2,18 @@ import time
 from celery import Celery
 
 
-celery = Celery('q01_celery', broker='redis://localhost:6379/0')
+app = Celery('q01_celery', broker='redis://localhost:6379/15')
 
 
-@celery.task
+@app.task
 def hello():
     time.sleep(2)
     return "hello world !!!"
 
 
-if __name__ == "__main__":
-    print(dir(celery.start()))
-    celery.star()
+# if __name__ == "__main__":
+#     print(dir(app.start()))
+#     app.star()
 
 
 # celery -A q01_celery worker --loglevel=info
